c9a598f8e2621074e57998d0e9648662dcead0b1
Gaurang Gaur <115665866+Gaurang-Gaur@users.noreply.github.com>
2023-01-24 20:31:34 +0530

    pro++ in dom

---
+++ dom/dommanipulation.js
@@ -0,0 +1,30 @@
+// // DOM stands for Document object models 
+// // In this we consider the document as the object ..
+// // dynamic change in html in runtime
+// // consider document as the object in js...
+// // let elme=document.getElementById('click');
+// // console.log(elme);
+// // let element=document.getElementsByClassName("container");
+// // console.log(element);
+// // element[0].style.background='yellow';
+// // element[0].classList.add("bg-primary");
+// // console.log(element[0].innerHTML);
+// // // console.log(elme.innerText);
+// // let tn=document.getElementsByTagName('div');
+// // console.log(tn);
+// // tn[0].style.background='blue';
+// // createdelement=document.createElement('p');
+// // createdelement.innerText="This is a created era";
+// // createdelement2=document.createElement('b');
+// // createdelement2.innerText="Bold text";
+// // tn[0].replaceChild(createdelement2,createdelement);
+// // // selecting using query..
+// sel=document.querySelectorAll('.container');
+// console.log(sel);
+
+
+
+
+
+
+
