3612c1518216a84e3395b764d02997096bd90ea0
Gaurang Gaur <115665866+Gaurang-Gaur@users.noreply.github.com>
2023-01-27 23:42:37 +0530

    adding youtube clone

---
+++ Dicechallenge/index.js
@@ -0,0 +1,33 @@
+// let a=0;
+// function randome(){
+//     a=Math.random()*6;
+//     a=Math.floor(a);
+//     a=a+1;
+//     // console.log(a);
+    
+//     return a;
+    
+// }
+
+// let b=0; 
+// function random(){
+//     b=Math.random()*6;
+//     b=Math.floor(b);
+//     b=b+1;
+//     // console.log(b);
+
+//     return b;
+
+
+// }
+// a=randome();
+// b=random();
+// // console.log(a);
+
+// document.getElementsByClassName("dice1").setAttribute("src",`style/images/dice${a}.png`);
+// document.getElementsByClassName("dice2").setAttribute("src",`style/images/dice${b}.png`);
+
+
+// alert("working");
+let a=document.getElementsByClassName("dice1");
+// setAttribute
